0ca6ca346aa70137484717a639ff5aa7b087af78
Lasse Bunk <lassebunk@gmail.com>
2012-05-28 09:09:31 +0200

    better way of handling custom meta tags

--- lib/metamagic/helper_methods.rb
+++ lib/metamagic/helper_methods.rb
@@ -1,38 +1,52 @@
 module Metamagic
   module HelperMethods
-    def meta(options = {})
-      @meta_tags = options
+    def meta_tags
+      @meta_tags ||= []
+    end
+    
+    def meta(*options)
+      # add page specific meta tags
+      add_meta_tags options
+    end
+    
+    def add_tag_if_not_existing(new_tag)
+      # add meta tag if it's not existing
+      unless meta_tags.find { |tag| tag[:name] && new_tag[:name] && tag[:name] == new_tag[:name] }
+        meta_tags << new_tag
+      end
+    end
+    
+    def add_meta_tags(options)
+      options.each do |option|
+        if option.is_a?(Hash)
+          option.each_pair do |key, value|
+            add_tag_if_not_existing :name => key, :content => value
+          end
+        elsif option.is_a?(Array)
+          option.each do |tag|
+            add_tag_if_not_existing tag
+          end
+        else
+          raise TypeError, "Unknown tag type #{tag.class.name}. Use either Hash or Array."
+        end
+      end
     end
 
-    def metamagic(options = {})
-      options = @meta_tags || options
-
+    def metamagic(*options)
+      # apply default meta tags if they don't exist
+      add_meta_tags options
+      
+      # loop through the added tags
       out = []
-      options.each do |key, value|
-        if key.to_s == "title"
-          out << content_tag(:title, value)
+      meta_tags.each do |tag|
+        if tag[:name] == :title
+          out << content_tag(:title, tag[:content])
         else
-          props = { :name => key.to_s }
-        
-          if value.is_a?(Hash)
-            props.merge! value
-          else
-            props[:content] = value
-          end
-        
-          # loop through each property to find
-          # arrays that need to be joined
-          props.each_pair do |propkey, propvalue|
-            if propvalue.is_a?(Array)
-              propvalue = propvalue.join(", ")
-            end
-            props[propkey] = propvalue
-          end
-        
-          out << tag("meta", props) if value
+          out << tag(:meta, tag)
         end
       end
       
+      # return tags
       out.join.html_safe
     end
   end
